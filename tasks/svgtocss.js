f6a7cab98399839bcd50bfc0fe9cd6a6dc57807e
kajyr <carlo.panzi@gmail.com>
2016-01-20 14:46:44 +0100

    inizializzazione

--- tasks/svgtocss.js
+++ tasks/svgtocss.js
@@ -4,7 +4,6 @@ module.exports = function(grunt) {
 
 	return grunt.registerMultiTask('svgtocss', 'Grunt plugin to convert svg to css', function() {
 
-		console.log(this.files)
 			this.files.forEach(function(file) {
 
 				
